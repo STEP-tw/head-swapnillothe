007fed0b8936c0f134e535d1c7eab5a9ce47645b
lswapnil <swapnillothe15@gmail.com>
2018-12-04 15:53:42 +0530

    Introduce a getFirstNCharacters function with tests.

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,12 @@
-const getNHeadLines = function( text, numberOfLines ){
-  let head = text.split('\n').filter( ( x, y ) => y < numberOfLines ).join('\n');
-  return head;
+const getNHeadLines = function( text, n ){
+  let head = text.split('\n').filter( ( x, y ) => y < n );
+  return head.join('\n');
 }
 
+const getFirstNCharacters = function( text, n ){
+  let firstNCharacters = text.slice( 0, n );
+  return firstNCharacters;
+}
+
+exports.getFirstNCharacters = getFirstNCharacters;
 exports.getNHeadLines = getNHeadLines;
--- test/libTest.js
+++ test/libTest.js
@@ -1,5 +1,5 @@
 const { deepEqual } = require( 'assert' );
-const { getNHeadLines } = require( '../src/lib.js' );
+const { getNHeadLines, getFirstNCharacters } = require( '../src/lib.js' );
 
 describe("getNHeadLines",function() {
   it("should work for no line",function() {
@@ -13,5 +13,18 @@ describe("getNHeadLines",function() {
   });
   it("should work for less no of lines with more requirment of no of lines",function() {
     deepEqual( getNHeadLines( 'a\nb\nc', 5 ), 'a\nb\nc' );
+  });
+});
+
+describe("getFirstNCharacters",function() {
+  it("should work for single line",function() {
+    deepEqual( getFirstNCharacters( "abc", 2 ), "ab" );
+  });
+  it("should work for multiple lines",function() {
+    deepEqual( getFirstNCharacters( "abc\nabc", 2 ), "ab" );
+    deepEqual( getFirstNCharacters( "abc\nabc", 5 ), "abc\na" );
+  });
+  it("should return empty string for zero character requirement",function() {
+    deepEqual( getFirstNCharacters( "abc", 2 ), "ab" );
   });
 });
