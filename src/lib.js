803572fe7ac1bec1644ad56bd8a7e6391f5face5
lswapnil <swapnillothe15@gmail.com>
2018-12-04 19:00:38 +0530

    Revert "Introduce a head function with test."
    This reverts commit fe32dbb25e00b4c7f3fded06c5aa7d0160e6adef.

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,18 @@
-const identifyAction = function( sign ){
-  let action = "getNHeadLines";
-  if( sign.match("-c") ){
-    action = "getFirstNCharacters";
-  };
-  return action 
+const getNHeadLines = function( n, text ){
+  let head = text.split('\n').filter( ( x, y ) => y < n );
+  return head.join('\n');
 }
 
-console.log( identifyAction("-c"),"getFirstNCharacters");
+const getFirstNCharacters = function( n, text ){
+  return text.slice( 0, n );
+}
+
+const head = function( { action, files, headLineNumbers } ){
+  let headFunc = action.bind( null, headLineNumbers );
+  let requiredHead = files.map( headFunc );
+  return requiredHead.join("\n");
+}
+
+exports.getFirstNCharacters = getFirstNCharacters;
+exports.getNHeadLines = getNHeadLines;
+exports.head = head;
