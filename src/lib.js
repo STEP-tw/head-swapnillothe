cd4ccb903407215429847738e3a2a9a5bd748f6d
lswapnil <swapnillothe15@gmail.com>
2018-12-04 19:00:24 +0530

    Revert "Introduce a head function with test."
    This reverts commit b84aa8f66507de2c4a79d07a41cbf5228a1408ca.

--- src/lib.js
+++ src/lib.js
@@ -6,13 +6,4 @@ const identifyAction = function( sign ){
   return action 
 }
 
-<<<<<<< HEAD
 console.log( identifyAction("-c"),"getFirstNCharacters");
-=======
-const getFirstNCharacters = function( n, text ){
-  return text.slice( 0, n );
-}
-
-exports.getFirstNCharacters = getFirstNCharacters;
-exports.getNHeadLines = getNHeadLines;
->>>>>>> parent of ceb1b6b... Introduce a head function with test.
