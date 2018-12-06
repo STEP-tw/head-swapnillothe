8fd5da9ecc63ab007e7d9f7e7f3ef74cdb448706
lswapnil <swapnillothe15@gmail.com>
2018-12-06 16:49:41 +0530

    Add main function to head.js file.

--- head.js
+++ head.js
@@ -1,20 +1,8 @@
-/* 
-  Usage:
-  node ./head.js file1
-  node ./head.js -n5 file1
-  node ./head.js -n 5 file1
-  node ./head.js -5 file1
-  node ./head.js file1 file2
-  node ./head.js -n 5 file1 file2
-  node ./head.js -n5 file1 file2
-  node ./head.js -5 file1 file2 
-  node ./head.js -c5 file1
-  node ./head.js -c 5 file1
-  node ./head.js -c5 file1 file2
-  node ./head.js -c 5 file1 file2
-*/
-
 const { readFileSync, existsSync }  = require('fs');
 const { head, readUserInputs } = require('./src/lib.js');
 
-console.log( head( readUserInputs( process.argv, readFileSync, existsSync ) ) );
+const main = function(){
+  console.log( head( readUserInputs( process.argv, readFileSync, existsSync ) ) );
+}
+
+main();
