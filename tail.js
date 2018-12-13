bd4517d02b516ead88716579853d5a05247fd76d
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2018-12-13 13:41:41 +0530

    Deleted info about assignment and add two test cases for tail.

--- tail.js
+++ tail.js
@@ -6,18 +6,3 @@ const main = function(){
 }
 
 main();
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
